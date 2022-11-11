d1ea2bb75d2786dbf99531bf2a913e9458e4f72e
Elie Cohen <ec92009@gmail.com>
2022-11-11 08:37:37 +0000

    Initial Commit

---
+++ Flipzilla/FlipzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipzillaApp.swift
+//  Flipzilla
+//
+//  Created by Elie Cohen on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FlipzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
